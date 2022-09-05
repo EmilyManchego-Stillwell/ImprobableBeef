61a470a3ee4eb13f077dea45207208057681fa64
Emily Manchego-Stillwell <emanchego85@gmail.com>
2022-09-04 20:06:47 -0600

    updated index.html and plots.js to practice using map() and filter() methods to extract data from the samples.json file and make them viewable through the web browser using my local host server. I also created the skill drill folder to practice creating listeners for a drop-down menu on the web browser. I also created the scripts.js file because it was mentioned in 12.4.1 as a file to use for practice.

--- plots.js
+++ plots.js
@@ -1,3 +1,17 @@
+// // This caputures the json data and prints it to the console
+// d3.json("data/samples.json").then(function(data){
+//     console.log(data);
+// });
+
+// // Extracting one item from the metadata key in the samples.json file
+// d3.json("data/samples.json").then(function(data) {
+//     wfreq = data.metadata.map(person => person.wfreq).sort((a,b) => b - a);
+//     filteredWfreq = wfreq.filter(element => element != null);
+//     console.log(filteredWfreq);
+// });
+
+// Skill Drill 12.3.2 print all the metadata of the first person in the samples.json() dataset(ID940)
 d3.json("data/samples.json").then(function(data){
-    console.log(data);
+    firstMetadataEntry = data.metadata[0];
+    Object.entries(firstMetadataEntry).forEach(([first,second])=> console.log(`${first}:${second}`));
 });
