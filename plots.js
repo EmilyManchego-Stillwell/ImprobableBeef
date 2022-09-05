165915fca7d9937fc1c7b7d6d6b46221149df5f9
Emily Manchego-Stillwell <emanchego85@gmail.com>
2022-09-05 15:32:35 -0600

    updated plots.js to show the demographic info based on the sample id that is selected.

---
+++ plots.js
@@ -0,0 +1,35 @@
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("data/samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })};
+  
+  init();
+
+  function optionChanged(newSample) {
+    // console.log(newSample);
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  };
+
+function buildMetadata(sample) {
+    d3.json("data/samples.json").then((data)=>{
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        console.log(resultArray);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
+
+        PANEL.html("");
+        Object.entries(result).forEach(([key,value])=> PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`));
+        // PANEL.append("h6").text(result.location);
+    });
+};
